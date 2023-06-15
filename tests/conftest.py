674149dc3650ea633286dd6cedb0b3393c2142c8
Yura <Orl0ff.1ur@yandex.ru>
2023-06-15 22:51:13 +0300

    test_calculate_total_price fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,13 @@ import pytest
 from src.item import Item
 @pytest.fixture
 def fixture_calculate_total_price():
-     item1 = Item("Смартфон", 10000, 20)
-     return item1
+    return 200000
+
+@pytest.fixture
+def fixture__total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    return item1
+
 
 @pytest.fixture
 def fixture_apply_discount():
