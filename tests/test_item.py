9bc86ad9cae43ae0a98c9a8fced0b7c4b1b670ba
Yura <Orl0ff.1ur@yandex.ru>
2023-06-15 22:34:58 +0300

    test_calculate_total_price

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,8 @@
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+def test_calculate_total_price(fixture_calculate_total_price):
+    assert fixture_calculate_total_price == 200000
+
+def test_apply_discount():
+    pass
